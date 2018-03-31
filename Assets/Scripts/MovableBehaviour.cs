a1b1df16be6dcf3b90babe05897e854a499bf9a9
Marcelo <marcelobonowott@yahoo.com.br>
2018-03-31 04:51:18 -0300

    Adicionado arquivos iniciais

---
+++ Assets/Scripts/MovableBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableBehaviour : MonoBehaviour {
+
+
+    public LayerMask layersToIgnoreCollision;
+    private new Collider collider;
+
+    private void Awake()
+    {
+        if(collider == null)
+            collider = GetComponent<Collider>();
+    }
+
+    public void MoveAdd(float x, float y)
+    {
+        float rayXPosition = x > 0 ? collider.bounds.max.x : collider.bounds.min.x;
+        Ray ray = new Ray(new Vector3(rayXPosition, transform.position.y), new Vector3(x, y, 0));
+        float maxDistance = Mathf.Max(Mathf.Abs(x), Mathf.Abs(y));
+        RaycastHit RayCastHitInfo;
+        int layers = int.MaxValue - layersToIgnoreCollision.value;
+        if(!Physics.Raycast(ray, out RayCastHitInfo, maxDistance, layers))
+        {
+            Debug.DrawLine(transform.position, new Vector3(rayXPosition+ x, transform.position.y + y, 0), Color.red, 0.5f);
+            transform.position = new Vector2(transform.position.x + x, transform.position.y + y);
+        }
+        else
+        {
+            float dumpPosition = transform.position.x - (x > 0 ? collider.bounds.max.x : collider.bounds.min.x);
+            transform.position = new Vector2(transform.position.x + dumpPosition, transform.position.y + y);
+        }
+       
+    }
+
+    public void Move(float x)
+    {
+        MoveAdd(x, 0);
+    }
+
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    [SerializeField] private MovableBehaviour behaviour;
+
+    [SerializeField] float speed;
+    private float horizontalInput;
+
+    private void Awake()
+    {
+        if(behaviour == null && (behaviour = GetComponent<MovableBehaviour>()) == null)
+        {
+            Debug.LogError("Nao tem movableBehaviour");
+            Application.Quit();
+        }
+    }
+
+    void Update () {
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        if(Mathf.Abs(horizontalInput) > 0.02)
+        {
+            behaviour.Move(horizontalInput * speed);
+        }
+        
+
+	}
+}
